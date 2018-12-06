c20b696043230761d4804ef867abf0c24d08f25e
Lukas <lukas.dirda@gmail.com>
2018-12-06 16:01:49 +0200

    small task in console log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import data from './data.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Aardvark-Roulette';
+  data;
+
+  ngOnInit(): void {
+    this.data = data;
+    this.getMeResult(null, this.data);
+  }
+
+  getMeResult(parent, node): void {
+    let pr = parent ? parent : node.name;
+    if (!parent && node.nodes.length > 0) {
+      console.log(pr + ' ' + node.size);
+    }
+
+    if (node.nodes.length > 0) {
+      for (const nd of node.nodes) {
+        pr = pr + ' - ' + nd.name;
+        console.log(pr  + ' ' + nd.size);
+        this.getMeResult(pr, nd);
+      }
+    }
+  }
 }
